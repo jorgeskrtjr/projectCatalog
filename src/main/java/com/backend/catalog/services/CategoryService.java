d57b30a8bb1c769642f1aef8e38920c3d0fdd018
Jorge jr <jorgesilvajunior10@gmail.com>
2023-01-09 15:50:00 -0300

    add transactional and database seeding

--- src/main/java/com/backend/catalog/services/CategoryService.java
+++ src/main/java/com/backend/catalog/services/CategoryService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.backend.catalog.entities.Category;
 import com.backend.catalog.repositories.CategoryRepository;
+
 
 @Service
 public class CategoryService {
@@ -14,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository; 
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		
 		return repository.findAll(); 
