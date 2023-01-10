e4858edc46a19afd7e5abf8589fab8a1488989ba
Jorge jr <jorgesilvajunior10@gmail.com>
2023-01-10 15:39:43 -0300

    add Exception handler

--- src/main/java/com/backend/catalog/services/CategoryService.java
+++ src/main/java/com/backend/catalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.backend.catalog.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.backend.catalog.dto.CategoryDTO;
 import com.backend.catalog.entities.Category;
 import com.backend.catalog.repositories.CategoryRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -30,5 +33,18 @@ public class CategoryService {
 		}
 		
 		return dtoList; 
+	}
+	
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		
+		Optional<Category> obj = repository.findById(id); 
+        
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found")); 
+		
+        CategoryDTO dto = new CategoryDTO(entity);
+        
+		return dto;
 	}
 }
